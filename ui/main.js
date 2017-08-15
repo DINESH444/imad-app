f3772daca96fda89589c50ac80dfc994a0040fab
Dinesh Manikanta Katta <dineshmanikanta.timothiraju@gmail.com>
2017-08-15 18:41:20 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,3 +6,11 @@ var element = document.getElementById('main-text')
 
 element.innerHTML = 'New Value'
 
+//move the image
+
+var img = document.getElementById('madi')
+
+img.onclick=function(){
+    img.style.marginleft= '100px';
+}
+
