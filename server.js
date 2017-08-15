4e1d0588a71bfd1fe7f4371a0665342939879ba7
Dinesh Manikanta Katta <dineshmanikanta.timothiraju@gmail.com>
2017-08-15 19:22:04 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -184,7 +184,7 @@ app.get('/ui/main.js', function (req, res) {
 });
 
 
-var counter=0;
+var counter = 0;
 app.get('/counter',function(req,res){
     counter = counter + 1;
     res.send(counter.toString());
