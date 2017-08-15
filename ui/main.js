aeb3934cb2470685a3ab93c373db6ee0bcf6d26e
Dinesh Manikanta Katta <dineshmanikanta.timothiraju@gmail.com>
2017-08-15 18:52:54 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -18,7 +18,7 @@ function moveRight(){
 }
 
 img.onclick=function(){
-    var interval = setInterval(moveRight,50);
+    var interval = setInterval(moveRight,10);
    // img.style.marginLeft= '100px';
 }
 
