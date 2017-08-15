15650b0a3c80ed110b7ca02b6f1b20ee0388f794
Dinesh Manikanta Katta <dineshmanikanta.timothiraju@gmail.com>
2017-08-15 19:55:31 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -27,7 +27,9 @@ img.onclick=function(){
 //Counter code
 
 var button = document.getElementById("counter");
+
 var counter= 0;
+
 button.oncclick = function(){
     counter= counter + 1;
     var span = document.getElementById('count');
