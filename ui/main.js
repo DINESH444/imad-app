f15324df3fc3f12717fb65946d9014ea36a726a7
Dinesh Manikanta Katta <dineshmanikanta.timothiraju@gmail.com>
2017-08-15 23:15:38 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,27 @@
+console.log('Loaded!');
+
+alert("Welcome to my Web-App");
+
+//var element = document.getElementById('main-text')
+
+//element.innerHTML = 'New Value'
+
+//move the image
+
+var img = document.getElementById('madi')
+
+var marginLeft= 0;
+
+function moveRight(){
+    marginLeft= marginLeft + 1;
+    img.style.marginLeft =marginLeft + 'px';
+}
+
+img.onclick=function(){
+    var interval = setInterval(moveRight,10);
+   // img.style.marginLeft= '100px';
+}
+
 
 
 //Counter code
