fd5883e5b84efdf32ffb3e31a01f9228b2ac2719
Dinesh Manikanta Katta <dineshmanikanta.timothiraju@gmail.com>
2017-08-18 12:17:29 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -39,31 +39,32 @@ button.onclick = function(){
         if (request.readystate === XMLHttpRequest.Done){
             
             if(request.status === 200){
-                var nameInput = document.getElementById('name');
-                var name = nameInput.value;
-                var submit = document.getElementById('submit_btn');
-                submit.onclick = function(){
-                var names = request.responseText;
-                names = JSON.parse(names);
-                var list = '';
-                for(var i=0; i<names.length; i++){
-                     list += '<li>'+ names[i] +'</li>';
-                }
-                    
-                var ul=document.getElementById('namelist');
-                ul.innerHTML = list;
+                var counter = request.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML = counter.toString();
             }
         }
     }
      
-request.open('GET','http://dineshmanikantatimothiraju58.imad.hasura-app.io/submit-name>name=' +name,true);
+request.open('GET','http://dineshmanikantatimothiraju58.imad.hasura-app.io/counter',true);
 request.send(null);
     
 }
 
 //submit name
 
-
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
+var submit = document.getElementById('submit_btn');
+submit.onclick = function(){
+    var names = ['name1','name2','name3','name4','name5'];
+    var list = '';
+    for(var i=0; i<names.length; i++){
+        list += '<li>'+ names[i] +'</li>';
+    }
+    
+    var ul=document.getElementById('namelist');
+    ul.innerHTML = list;
 };
 
 
