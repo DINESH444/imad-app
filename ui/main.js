cc7310a6001ee4416f0683a4ee1f46e0b1350da8
Dinesh Manikanta Katta <dineshmanikanta.timothiraju@gmail.com>
2017-08-18 10:45:17 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -33,11 +33,21 @@ var button = document.getElementById("counter");
 
 button.oncclick1 = function(){
     
+    var request = new XMLHttpRequest();
     
-    counter= counter + 1;
+    request.onreadystatechange = function(){
+        
+        if (request.readystate === XMLHttpRequest.Done){
+            
+            if(request.status === 200){
+                var counterv= request.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML = counter.toString();
+            }
+        }
+    }
      
-    var span = document.getElementById("count");
-    
-    span.innerHTML = counter.toString();
+request.open('GET','http://dineshmanikantatimothiraju58.imad.hasura-app.io/counter',true);
+request.send(null);
     
 }
