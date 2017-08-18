0eddbcb0188301c0ca950ccdf39bca6eaa16e7d5
Dinesh Manikanta Katta <dineshmanikanta.timothiraju@gmail.com>
2017-08-18 11:44:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -177,6 +177,13 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
+app.get('/submit-name/:name',function(req,res){
+    var name = req.params.name;
+    names.push(name);
+    res.send(names);
+    
+})
+
 app.get('/:articleName',function(req,res){
     var articleName = req.params.articleName;
     res.send(createTemplate(Articles[articleName]));
