262a629a8a335b2a744f601223c7cd9f9e2d873d
Dinesh Manikanta Katta <dineshmanikanta.timothiraju@gmail.com>
2017-08-18 10:53:52 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -40,7 +40,7 @@ button.onclick = function(){
         if (request.readystate === XMLHttpRequest.Done){
             
             if(request.status === 200){
-                var counterv= request.responseText;
+                var counter = request.responseText;
                 var span = document.getElementById('count');
                 span.innerHTML = counter.toString();
             }
