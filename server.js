1c9958a0868d7f0e1e33ffdaf4578a05705c16ea
Dinesh Manikanta Katta <dineshmanikanta.timothiraju@gmail.com>
2017-08-22 17:12:44 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ var path = require('path');
 var Pool = require('pg').Pool;
 
 var config = {
-    user: 'coco98',
-    database: 'coco98',
+    user: 'dineshmanikantatimothiraju58',
+    database: 'dineshmanikantatimothiraju58',
     host: 'http://db.imad.hasura-app.io',
     port: '5432',
     password: process.env.DB_PASSWORD
