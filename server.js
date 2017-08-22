2658dc50684a73814db5d280b7a0efd9129a210f
Dinesh Manikanta Katta <dineshmanikanta.timothiraju@gmail.com>
2017-08-22 17:02:23 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var config = {
     database: 'dineshmanikantatimothiraju58',
     host: 'http://db.imad.hasura-app.io',
     port: '5432',
-    password: process.env.DB_PASSWORD
+    password: db-dineshmanikantatimothiraju58-32402
     
 };
 var app = express();
