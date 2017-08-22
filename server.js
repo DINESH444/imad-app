4859d09992bdb4e4b4ec1cb7f59aa9d07659802d
Dinesh Manikanta Katta <dineshmanikanta.timothiraju@gmail.com>
2017-08-22 17:00:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,6 +2,16 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 
+var Pool = require('pg').Pool;
+
+var config = {
+    user: 'dineshmanikantatimothiraju58',
+    database: 'dineshmanikantatimothiraju58',
+    host: 'http://db.imad.hasura-app.io',
+    port: '5432',
+    password: process.env.DB_PASSWORD
+    
+};
 var app = express();
 app.use(morgan('combined'));
 
@@ -184,8 +194,21 @@ app.get('/counter', function(req, res){
     counter = counter + 1;
     res.send(counter.toString());
     
-}); 
+});
 
+var pool = new Pool(config);
+app.get('/test-db', function(req,res){
+    
+    pool.query('select*from test', function(err,result){
+        if(err){
+            res.status(500).send(err.toString());
+        }else{
+            res.send(JSON.stringify(result));
+        }
+        
+    });
+    
+});
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
