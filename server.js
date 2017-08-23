ff6d1506271af3193502d7bc699e4dfc8629b253
Dinesh Manikanta Katta <dineshmanikanta.timothiraju@gmail.com>
2017-08-23 12:46:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -224,12 +224,23 @@ var names=[];
 app.get('/submit-name/',function(req,res){
     var name = req.query.name;
     names.push(name);
-    res.send(JSON.stringify(names))
+    res.send(JSON.stringify(names));
     
 });
 
-app.get('/:articleName',function(req,res){
-    var articleName = req.params.articleName;
+app.get('/articles/:articleName',function(req,res){
+    pool.query("select * from article where title = ' "+ req.params.articleName +"'" ,function(err,result){
+        if(err){
+            res.status(500).send(err.toString());
+        } else{
+            if(result.rows.length === 0){
+                res.status(404).send("Article not found");
+            } else{
+                var articleData = result.rows[0];
+                res.send(createTemplate(articleData));
+            }
+        }
+    });
     res.send(createTemplate(Articles[articleName]));
 });
 
