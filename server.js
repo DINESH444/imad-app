4f15f3782de225434a7c8d157cabadf7abdb9d97
Dinesh Manikanta Katta <dineshmanikanta.timothiraju@gmail.com>
2017-08-28 16:54:49 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -62,6 +62,19 @@ app.get('/', function (req, res) {
 });
 
 
+function hash (input, salt){
+    var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
+    return hashed.toString('hex');
+}
+
+
+app.get('/hash/:input', function(req,res){
+    var hashedString = hash(req.params.input, 'this-is-some-random-input');
+    res.send(hashedString);
+});
+
+
+
 var pool = new Pool(config);
 app.get('/test-db', function(req,res){
     
@@ -83,17 +96,6 @@ app.get('/counter', function(req, res){
     
 });
 
-
-function hash (input, salt){
-    var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
-    return hashed.toString('hex');
-}
-
-
-app.get('/hash/:input', function(req,res){
-    var hashedString = hash(req.params.input, 'this-is-some-random-input');
-    res.send(hashedString);
-});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
