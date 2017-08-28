e5fad99ee92c1110a9585a2474dcdcdb2185fdb0
Dinesh Manikanta Katta <dineshmanikanta.timothiraju@gmail.com>
2017-08-28 16:48:56 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -58,7 +58,7 @@ function createTemplate(data){
 
 
 function hash (input, salt){
-    var hashed = crypto.pbkdfSync(input, salt, 10000, 512, 'sha512');
+    var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
     return hashed.toString('hex');
 }
 
