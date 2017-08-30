be62967f4b18019b95a66c4be3699ed46ad2f1cf
Dinesh Manikanta Katta <dineshmanikanta.timothiraju@gmail.com>
2017-08-30 13:16:33 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -31,7 +31,7 @@ submit.onclick = function(){
     var password = document.getElementById('password').value;
     console.log(username);
     console.log(password);
-    request.open('get','http://dineshmanikantatimothiraju58.imad.hasura-app.io/login', true);
+    request.open('POST','http://dineshmanikantatimothiraju58.imad.hasura-app.io/login', true);
     request.setRequestHeader('Content-Type','application/json');
     request.send(JSON.stringify({"username": username, "password": password}));
 
