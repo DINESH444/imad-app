05800e684d184e756e2274098f9c1b010d0032cf
Dinesh Manikanta Katta <dineshmanikanta.timothiraju@gmail.com>
2017-08-30 11:53:20 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -86,6 +86,40 @@ request.send(null);
 };
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+//submit-login details
+
+var submit = document.getElementById('submit_btn');
+submit.onclick = function(){
+    
+    var nameInput = document.getElementById('name');
+var name = nameInput.value;
+     var request = new XMLHttpRequest();
+    
+    request.onreadystatechange = function(){
+        
+        if (request.readystate === XMLHttpRequest.Done){
+            
+            if(request.status === 200){
+               var names = request.responseText;
+               names = JSON.parse(names);
+                var list = '';
+                for(var i=0; i<names.length; i++){
+                    list += '<li>'+ names[i] +'</li>';
+                }
+                
+                var ul=document.getElementById('namelist');
+                ul.innerHTML = list;
+            }
+        }
+    }
+     
+request.open('GET','http://dineshmanikantatimothiraju58.imad.hasura-app.io/submit-name?name=' +name,true);
+request.send(null);
+
+    
+};
+
+
 
 
 
